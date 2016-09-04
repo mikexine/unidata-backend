d69e23ea05648fd069496335b1d82bcbbf89aa0a
Michele Colombo <mikexine@gmail.com>
2016-09-04 16:48:14 +0200

    introduce migrations

--- uniopen/app.py
+++ uniopen/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.assets import Environment
 from webassets.loaders import PythonLoader
-
+from flask_migrate import Migrate
 from uniopen import assets
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ app.config.from_object('uniopen.settings.{env}Config'
                         .format(env=env.capitalize()))
 app.config['ENV'] = env
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Register asset bundles
 assets_env = Environment()
